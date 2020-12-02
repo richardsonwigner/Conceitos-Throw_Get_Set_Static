c73c8e1210a2bab808a4d8b574eb88c395204496
richardson wigner <richardsonman@hotmail.com>
2020-12-01 22:53:08 -0300

    .

---
+++ Conceitos Throw_Get_Set_Static/Empregado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conceitos_Throw_Get_Set_Static
+{
+    class Empregado
+    {
+        string primeiro_nome;
+        string sobrenome;
+        double salario;
+        static Exception ExceptionSalario = new Exception("Salario não pode ser negativo");
+        public Empregado(string primeiro_nome, string sobrenome, double salario)
+        {
+            this.primeiro_nome = primeiro_nome;
+            this.sobrenome = sobrenome;
+            this.salario = salario;
+            if(salario < 0)
+            {
+                throw ExceptionSalario;   
+            }
+        }
+        public void SalarioEmpregado()
+        {
+            Console.WriteLine($"Salario atual:{salario}");
+        }     
+        public void SalarioAtualizado()
+        {
+            Console.WriteLine($"Salario Com aumento:{salario}");
+        }
+
+        public string PrimeiroNome 
+        {
+            get { return primeiro_nome; }
+            set { primeiro_nome = value; }
+        }
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+        public double Salario
+        {
+            get { return salario; }
+            set { salario = value; }
+        }
+
+
+    }
+}
---
+++ Conceitos Throw_Get_Set_Static/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conceitos_Throw_Get_Set_Static
+{
+    class Program
+    {
+        static Empregado empregado = new Empregado("Pedro", "Silva", 5000);
+        static void Main(string[] args)
+        {
+            empregado.SalarioEmpregado();
+            SalarioAtualizado();
+            empregado.SalarioAtualizado();
+        }
+        static void SalarioAtualizado()
+        {
+            empregado.Salario = empregado.Salario * 1.10;
+        }
+    }
+}
